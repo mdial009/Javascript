be7f77f6cbd0935e13e529f9ea531cee9a7a1584
Madany Diallo <madanydiallo@Madanys-MacBook-Pro.local>
2020-07-24 14:58:01 -0400

    Initial Commit

---
+++ Projects/NavBar/app.js
@@ -0,0 +1,16 @@
+// classlist - show/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggle class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function (e) {
+  if (links.classList.contains("show-links")) {
+    links.classList.remove("show-links");
+  } else {
+    links.classList.add("show-links");
+  }
+});
